b68c4aefbcb94d3f2260c78e29390e79931fdb27
Oriol Martinez <omartinez.roser@gmail.com>
2021-11-29 14:56:17 +0100

    delay experiment

---
+++ delay/acceptor.erl
@@ -0,0 +1,57 @@
+-module(acceptor).
+-export([start/2]).
+-define(delay, 200).
+
+start(Name, PanelId) ->
+  spawn(fun() -> init(Name, PanelId) end).
+        
+init(Name, PanelId) ->
+  Promised = order:null(), 
+  Voted = order:null(),
+  Value = na,
+  acceptor(Name, Promised, Voted, Value, PanelId).
+
+acceptor(Name, Promised, Voted, Value, PanelId) ->
+  receive
+    {prepare, Proposer, Round} ->
+      case order:gr(Round, Promised) of
+        true ->
+		  T=rand:uniform(?delay),
+		  timer:send_after(T, Proposer, {promise, Round, Voted, Value})
+          %Proposer ! {promise, Round, Voted, Value},               
+      io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
+                 [Name, Round, Voted, Value]),
+          % Update gui
+          Colour = case Value of na -> {0,0,0}; _ -> Value end,
+          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
+                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+          acceptor(Name, Round, Voted, Value, PanelId);
+        false ->
+          Proposer ! {sorry, {prepare, Round}},
+          acceptor(Name, Promised, Voted, Value, PanelId)
+      end;
+    {accept, Proposer, Round, Proposal} -> %Round: ballot value, Proposal: the value within the ballot
+      case order:goe(Round, Promised) of
+        true ->
+		  T=rand:uniform(?delay),
+		  timer:send_after(T, Proposer, {vote, Round}),
+          %Proposer ! {vote, Round},
+          case order:goe(Round, Voted) of
+            true ->
+      io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
+                 [Name, Promised, Round, Proposal]),
+              % Update gui
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
+                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              acceptor(Name, Promised, Round, Proposal, PanelId);
+            false ->
+              acceptor(Name, Promised, Voted, Value, PanelId)
+          end;                            
+        false ->
+          Proposer ! {sorry, {accept, Round}},
+          acceptor(Name, Promised, Voted, Value, PanelId)
+      end;
+    stop ->
+      PanelId ! stop,
+      ok
+  end.
